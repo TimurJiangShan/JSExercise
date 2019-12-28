66483d1afeb270dc2cbbab86c9f3bbe6b246172f
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2019-12-28 19:25:12 +1100

    solved

--- template-strings/ex.js
+++ template-strings/ex.js
@@ -1,10 +1,21 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	let result = "";
+	for(let i = 0; i < strings.length; i++){
+		if(i > 0){
+			result += String(values[i-1]).toUpperCase();
+		}
+		result += strings[i];
+	}
+	return result;
+}
 
 var name = "kyle",
 	twitter = "getify",
 	topic = "JS Recent Parts";
 
 console.log(
-	`Hello ${name.toUpperCase()} (@${twitter.toUpperCase()}), welcome to ${topic.toUpperCase()}!` ===
+	upper`Hello ${name} (@${twitter}), welcome to ${topic}!` ===
 	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
 );
+
+// String array will always have one more value in it than the value array would have.
