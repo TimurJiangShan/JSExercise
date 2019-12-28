d252b09ea66d9e4e2c61002d9e9cdc4bbe037edb
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2019-12-28 18:37:09 +1100

    solved

--- template-strings/ex.js
+++ template-strings/ex.js
@@ -5,6 +5,6 @@ var name = "kyle",
 	topic = "JS Recent Parts";
 
 console.log(
-	`Hello ____ (@____), welcome to ____!` ===
+	`Hello ${name.toUpperCase()} (@${twitter.toUpperCase()}), welcome to ${topic.toUpperCase()}!` ===
 	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
 );
