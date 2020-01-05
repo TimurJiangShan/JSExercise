4fd94a54b77f14dd6f8b7aeeea37270d7921437b
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-01-05 19:06:13 +1100

    state

--- hooks-examples/src/State.js
+++ hooks-examples/src/State.js
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// const StateComponent = () => {
+//   const [isGreen, setIsGreen] = useState(true);
+
+//   return (
+//     <h1
+//       onClick={() => setIsGreen(!isGreen)}
+//       style={{ color: isGreen ? "limegreen" : "crimson" }}
+//     >
+//       useState Example
+//     </h1>
+//   );
+// };
+
+// export default StateComponent;
+import React, { useState } from 'react';
 
 const StateComponent = () => {
   const [isGreen, setIsGreen] = useState(true);
 
   return (
-    <h1
+    <h1 
       onClick={() => setIsGreen(!isGreen)}
-      style={{ color: isGreen ? "limegreen" : "crimson" }}
+      style={{ color: isGreen? "limegreen" : "crimson"}}
     >
       useState Example
     </h1>
   );
-};
+}
 
 export default StateComponent;
